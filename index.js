910c2da8fe17e3ece4588c54a10212eb9ca1b8c5
Danielle <dmiz@brandeis.edu>
2020-02-08 21:18:33 -0500

    second

---
+++ index.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", event => {
+  console.log("DOM fully loaded and parsed");
+  formHandler();
+});
+
+const formHandler = () => {
+  form = document.querySelector(".form");
+  form.addEventListener("submit", e => handleFormSubmission(e));
+};
+
+const handleFormSubmission = e => {
+  e.preventDefault();
+  let first=e.target.first.value 
+  let last= e.target.last.value 
+  let email= e.target.email.value 
+  console.log(first)
+};
